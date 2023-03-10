541e51803275ff6421b714d25876d9498c41ab4c
ForwardFuture <48513897+ForwardFuture@users.noreply.github.com>
2023-03-10 18:16:25 +0800

    Add files via upload

---
+++ car.h
@@ -0,0 +1,9 @@
+using namespace std;
+struct Car{
+	int workbench,goods;//所处工作台编号，携带物品编号 
+	double timerate,hitrate;//时间价值系数，碰撞价值系数 
+	double vx,vy,w;//二维线速度向量，角速度 
+	double ang,x,y;//朝向角度，坐标 
+	Car(double,double);
+	pair<double,double> mov(double,double);
+};
