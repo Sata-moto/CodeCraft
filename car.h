69544f9b57b4938a61251c90d9f922395163fce5
W X <2984854964@qq.com>
2023-03-13 16:25:20 +0800

    11

--- car.h
+++ car.h
@@ -2,9 +2,12 @@
 
 struct Car
 {
-	int workbench, goods;	  // 所处工作台编号，携带物品编号
-	double timerate, hitrate; // 时间价值系数，碰撞价值系数
-	double vx, vy, w;		  // 二维线速度向量，角速度
-	double ang, x, y;		  // 朝向角度，坐标
-	pair<double, double> mov(double, double);
+	int workbench, goods;	                 // 所处工作台编号，携带物品编号
+	double timerate, hitrate;                // 时间价值系数，碰撞价值系数
+	double vx, vy, w;		                 // 二维线速度向量，角速度
+	double ang, x, y;		                 // 朝向角度，坐标
+	double CalcAng(double, double);          // 计算当前朝向与目标点的偏向角
+	double CalcForward(double);              // 根据偏向角计算前进速度
+	void MarginCheck(double&);                      // 
+	pair<double, double> mov(double, double);// 将小车向目标点移动
 };
