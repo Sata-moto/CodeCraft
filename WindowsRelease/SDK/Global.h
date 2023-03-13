9fbaee65baa1c92f79af802b4d012dc6f3bbda68
W X <2984854964@qq.com>
2023-03-13 22:03:57 +0800

    1

---
+++ WindowsRelease/SDK/Global.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+const int N = 102;
